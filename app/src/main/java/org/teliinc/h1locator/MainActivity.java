fe24e13be05fddb6d57040674c28e0d1809c8879
chris.teli <chris.teli@gmail.com>
2015-12-11 14:07:12 -0800

    Added socket to get Lat/Long from the host.

--- app/src/main/java/org/teliinc/h1locator/MainActivity.java
+++ app/src/main/java/org/teliinc/h1locator/MainActivity.java
@@ -2,39 +2,60 @@ package org.teliinc.h1locator;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int SERVERPORT = 38301;
     // Mock location
     MockLocationProvider mock;
-    String s_latitude="21.44";
-    String s_longitude="-157.78";
+    // UI Binding
+    @Bind(R.id.longitude)
+    EditText etLongitude;
+    @Bind(R.id.latitude)
+    EditText etLatitude;
+    Handler updatePositionInformation;
 
-    @Bind(R.id.longitude) EditText etLongitude;
-    @Bind(R.id.latitude) EditText etLatitude;
+    Thread serverThread = null;
+    // Socket Initialization
+    private ServerSocket serverSocket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Bind with butterknife
         ButterKnife.bind(this);
 
+        // Initialize UI
         etLongitude.setText("-117.42");
         etLatitude.setText("33.33");
+
+        // Initialize Threading requirements
+        updatePositionInformation = new Handler();
+        this.serverThread = new Thread(new ServerThread());
+
     }
 
     @Override
@@ -80,4 +101,71 @@ public class MainActivity extends AppCompatActivity {
     public void stopService(View view) {
         stopService(new Intent(getBaseContext(), MockLocationProvider.class));
     }
+
+    public void connectToHost(View view) {
+        this.serverThread.start();
+    }
+
+    class ServerThread implements Runnable {
+
+        public void run() {
+            Socket socket = null;
+            try {
+                serverSocket = new ServerSocket(SERVERPORT);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    socket = serverSocket.accept();
+                    CommunicationThread commThread = new CommunicationThread(socket);
+                    new Thread(commThread).start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    class CommunicationThread implements Runnable {
+
+        private Socket clientSocket;
+        private BufferedReader input;
+
+        public CommunicationThread(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+            try {
+                this.input = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void run() {
+
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    String read = input.readLine();
+                    Log.i("Client Read", read);
+                    updatePositionInformation.post(new updateUIPosotion(read));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    class updateUIPosotion implements Runnable {
+        private String msg;
+
+        public updateUIPosotion(String str) {
+            this.msg = str;
+        }
+
+        @Override
+        public void run() {
+            etLongitude.setText(msg);
+        }
+    }
+
 }
